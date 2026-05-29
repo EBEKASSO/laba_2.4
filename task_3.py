f238c6f2f673c86590edebc9e5e5abbdd4e4a5f5
STEPAN <venomxv85@gmail.com>
2026-05-29 16:26:28 +0700

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,34 +1,32 @@
-def search(data):
-    result = []
-    for i in data:
-        if i % 7 == 0:
-            result.append(i)
-    return result
-
 try:
     numbers = list(map(int, input("Введите числа через пробел: ").split()))
-    multiples = search(numbers)
-
-    with open("resource/count.txt", "w") as file:
-        for num in numbers:
-            file.write(str(num) + " ")
 except ValueError:
     print("Ошибка: введите числа!")
+    exit()
 
-x = 73 ** 2 + 29
+# 1. Записываем исходные числа
+with open("resource/count.txt", "w") as file:
+    file.write(" ".join(map(str, numbers)) + "\n")
+
+# 2. Константа
+x = 73 ** 2 + 29  # 5358
 temp_path = "resource/count_tmp.txt"
 
-with open("resource/count.txt", "r") as file, open(temp_path, "w") as temp:
-    for line in file:
+# 3. Читаем -> обрабатываем -> пишем во временный файл
+with open("resource/count.txt", "r") as src, open(temp_path, "w") as tmp:
+    for line in src:
         processed = []
-        for j in line.split():
-            num = int(j)
+        for token in line.split():
+            num = int(token)
             if num % 7 == 0:
-                num = num * 100 / x
+                # Округляем до 2 знаков для читаемости
+                num = round(num * 100 / x, 2)
             processed.append(str(num))
-        temp.write(" ".join(processed) + "\n")
+        tmp.write(" ".join(processed) + "\n")
 
-    with open(temp_path, "r") as temp_path, open("resource/count.txt", "w") as file:
-        for line in temp_path:
-            temp.write(line)
+# 4. Заменяем исходный файл обработанным (выполняется ПОСЛЕ закрытия файлов выше)
+with open(temp_path, "r") as tmp, open("resource/count.txt", "w") as dst:
+    for line in tmp:
+        dst.write(line)
 
+print("Файл успешно обработан.")
